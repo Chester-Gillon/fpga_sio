68c65b8aa12f85db5f332f0144c9fce2467c32e0
Chester Gillon <chester.gillon@metronet.co.uk>
2023-11-11 20:36:12 +0000

    For the NiteFury_dma_ddr3 and TEF1001_dma_ddr3 FPGA designs: a. Add readback of USR_ACCESSE2 over PCIe b. Store the generated bitstreams under GIT control

--- NiteFury/constraints/spi.xdc
+++ NiteFury/constraints/spi.xdc
@@ -27,6 +27,7 @@ set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]
 set_property CONFIG_MODE SPIx4 [current_design]
 set_property BITSTREAM.CONFIG.SPI_FALL_EDGE YES [current_design]
 set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
+set_property BITSTREAM.CONFIG.USR_ACCESS TIMESTAMP [current_design]
 
 set_property CONFIG_VOLTAGE 3.3 [current_design]
 set_property CFGBVS VCCO [current_design]
--- multiple_boards/program_bitstream.tcl
+++ multiple_boards/program_bitstream.tcl
@@ -1,6 +1,12 @@
 # Program a bitsteam into a FPGA. 
 # Designed to be sourced with the Xilinx Vivado lab tools <install_root>/Vitis/<version>/bin/vivado_lab.
 # Takes a single argument which is the bitstream filename.
+#
+# Doesn't validate the bitstream matches the target device, since program_hw_devices does that.
+# Example error attempting to program an incompatible target device:
+#    ERROR: [Labtools 27-3303] Incorrect bitstream assigned to device.
+#    Bitfile is incompatible for this device.
+#    Bitstream was generated for device xc7k160t and target device is xc7a200t.
 
 if { $argc != 1 } {
     puts [concat "Usage: " $argv0 " <bitstream_filename>"]
@@ -21,7 +27,8 @@ if {[llength [get_hw_servers -quiet]] == 0 } then {
 # update list of targets
 refresh_hw_server -quiet
 
-if {[llength [get_hw_targets -quiet]] == 1 } then {
+set NUM_TARGETS [llength [get_hw_targets -quiet]]
+if {$NUM_TARGETS == 1 } then {
     set TARGET [get_hw_targets]
     open_hw_target -quiet $TARGET
     set DEVICE [get_hw_devices]
@@ -32,7 +39,8 @@ if {[llength [get_hw_targets -quiet]] == 1 } then {
     set_property PROGRAM.FILE $bitstream_filename [current_hw_device]
     program_hw_devices $DEVICE
 } else {
-    puts "Can only operate on a single target as no way of specifying the which target to load the bitstream to"
+    puts "Can only operate on a single target as no way of specifying the which target to load the bitstream to."
+    puts [concat "Number of targets : " $NUM_TARGETS]
 }
 
 if {$disconnect_en == 1 } {
