8260e85462d465d280c3f168b45f03a276b3c93e
Chester Gillon <chester.gillon@metronet.co.uk>
2023-11-12 11:52:18 +0000

    Add program_bitstream.bat for use under Windows. No changes were required to the program_bitstream.tcl which was originally developed under Linux.

---
+++ multiple_boards/program_bitstream.bat
@@ -0,0 +1,34 @@
+@echo off
+rem Wrapper script to call program_bitstream.tcl using the Vivado lab tools under Windows
+
+rem Get the absolute path of this script (has ending backslash)
+set batdir=%~dp0
+
+set vivado_version=2021.1
+set vivado_lab_exe=C:\Xilinx\Vitis\%vivado_version%\bin\vivado_lab
+if not exist "%vivado_lab_exe%" (
+   echo Error: %vivado_lab_exe% doesn't exist
+   goto exit
+)
+
+rem Parse command line arguments.
+rem Simply checks that the supplied bitstream filename exists; doesn't verify that an actual bitstream file.
+if "%~1"=="" (
+   echo Usage: %0 ^<bitstream_filename^>
+   goto exit
+)
+
+set bitstream_filename="%1"
+if not exist "%bitstream_filename%" (
+   echo Error: %bitstream_filename% doesn't exist
+   goto exit
+)
+
+rem Call the Vivado lab tool to run the TCL script to perform the programming of the supplied bitstream filename.
+rem The lab tool arguments are set to disable the writing of log and journal files.
+rem
+rem -notrace suppresses the echoing of the TCL commands. 
+rem Not shown in the vivado_lab help, but found in https://support.xilinx.com/s/article/46102?language=en_US
+"%vivado_lab_exe%" -notrace -nolog -nojournal -mode batch -source "%batdir%program_bitstream.tcl" -tclargs "%bitstream_filename%"
+
+:exit
