c761a962399192417da7f99cb4c64f8a9f64bb74
Chester Gillon <chester.gillon@metronet.co.uk>
2025-07-05 18:25:10 +0100

    Add NiteFury_dma_stream_crc64 FPGA design
    Modify XCKU5P_DUAL_QSFP_dma_stream_crc64 design to use the same CRC64 VHDL generator as the other *dma_stream_crc64 designs. The updated XCKU5P_DUAL_QSFP_dma_stream_crc64 design hasn't yet been tested.

--- fpga_tests/XCKU5P_DUAL_QSFP_dma_stream_crc64/stream_crc.vhd
+++ fpga_tests/XCKU5P_DUAL_QSFP_dma_stream_crc64/stream_crc.vhd
@@ -9,14 +9,11 @@
 -- Target Devices: 
 -- Tool Versions: 
 -- Description: 
---  A 64-bit AXI stream which always operates on 8 bytes at a time, and produces a single
+--  A 256-bit AXI stream which always operates on 32 bytes at a time, and produces a single
 --  8 byte CRC packet across the each input packet.
 -- Dependencies: 
---  crc_imp.vhd which was generated from http://outputlogic.com/?page_id=321
---  selecting the CRC-64-ECMA polynomial on https://en.wikipedia.org/wiki/Cyclic_redundancy_check
---
---  A block diagram is used to adapt the 8 byte wide interface of this component to a 32 byte wide
---  interface for the DMA Bridge.
+--  crc_imp.vhd which was generated from https://git.bues.ch/git/crcgen.git using:
+--    ./crcgen -V -a CRC-64-ECMA -b 256 -D data_in -C crc_in -o crc_out
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
@@ -41,22 +38,23 @@ use crc_imp.all;
 entity stream_crc is
     Port ( aclk : in STD_LOGIC;
            aresetn : in STD_LOGIC;
-           s_tdata : in STD_LOGIC_VECTOR (63 downto 0);
+           s_tdata : in STD_LOGIC_VECTOR (255 downto 0);
            s_tlast : in STD_LOGIC;
            s_tready : out STD_LOGIC;
            s_tvalid : in STD_LOGIC;
-           m_tdata : out STD_LOGIC_VECTOR (63 downto 0);
+           m_tdata : out STD_LOGIC_VECTOR (255 downto 0);
            m_tlast : out STD_LOGIC;
            m_tready : in STD_LOGIC;
            m_tvalid : out STD_LOGIC;
-           m_tkeep : out STD_LOGIC_VECTOR (7 downto 0));
+           m_tkeep : out STD_LOGIC_VECTOR (31 downto 0));
 end stream_crc;
 
 architecture Behavioral of stream_crc is
 
-    signal crc_rst : std_logic;
-    signal crc_en  : std_logic;
-    signal crc_out : std_logic_vector (63 downto 0);
+    signal crc_rst      : std_logic;
+    signal crc_en       : std_logic;
+    signal previous_crc : std_logic_vector (63 downto 0);
+    signal new_crc      : std_logic_vector (63 downto 0);
 begin
     -- Update the CRC when the input data is valid
     crc_en <= m_tready and s_tvalid;
@@ -64,32 +62,39 @@ begin
     crc_sub: entity work.crc
         port map (
         data_in => s_tdata,
-        crc_en => crc_en,
-        rst => crc_rst,
-        clk => aclk,
-        crc_out => crc_out
+        crc_in => previous_crc,
+        crc_out => new_crc
         );    
 
-    -- Don't accept input unless the output is ready
-    s_tready <= m_tready;
+    -- Don't accept input if either:
+    -- a. The output isn't ready.
+    -- b. The CRC sequence is being reset.
+    s_tready <= m_tready and (not crc_rst);
 
     -- The output is always only a single word.
     m_tlast <= '1';
     
-    -- Pass out the current CRC
-    m_tdata <= crc_out;
+    -- Pass out the current 64-bit CRC on the least significant bits 
+    m_tdata(63 downto 0) <= previous_crc;
+    m_tdata(255 downto 64) <= (others => '0');
     
-    -- Output has a tkeep since the external 8-to-32 byte width convertor needs to preserve the 8 byte
-    -- packet size.
-    m_tkeep <= (others => '1');
+    -- Indictate only a 8 byte packet output size.
+    m_tkeep(7 downto 0) <= (others => '1');
+    m_tkeep(31 downto 8) <= (others => '0');
     
     process (aclk) begin
         if (aclk'EVENT and aclk = '1') then
             -- Reset the CRC sequence on AXI reset or end of the input stream
             crc_rst <= (not aresetn) or s_tlast;
+            
+            if (crc_rst = '1') then
+                previous_crc <= (others => '1');
+            elsif (crc_en = '1') then
+                previous_crc <= new_crc;
+            end if; 
 
             -- Output the CRC at the end of the input stream.
-            m_tvalid <= s_tlast;
+            m_tvalid <= crc_en and s_tlast;
         end if;
     end process;
 
