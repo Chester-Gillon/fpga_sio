eca6a8f5435d27ff999f09f831cf438ef9105fbd
Chester Gillon <chester.gillon@metronet.co.uk>
2025-12-17 10:52:41 +0000

    Add support for programming a device image into a Versal device

--- multiple_boards/program_bitstream.tcl
+++ multiple_boards/program_bitstream.tcl
@@ -1,15 +1,20 @@
-# Program a bitstream into a FPGA. 
+# Program a bitstream or device image into a FPGA. 
 # Designed to be sourced with the Xilinx Vivado lab tools <install_root>/Vitis/<version>/bin/vivado_lab.
-# Takes a single argument which is the bitstream filename.
+# Takes a single argument which is the bitstream or device image filename.
 #
-# Doesn't validate the bitstream matches the target device, since program_hw_devices does that.
+# Doesn't validate the bitstream or device image matches the target device, since program_hw_devices does that.
 # Example error attempting to program an incompatible target device:
 #    ERROR: [Labtools 27-3303] Incorrect bitstream assigned to device.
 #    Bitfile is incompatible for this device.
 #    Bitstream was generated for device xc7k160t and target device is xc7a200t.
+#
+#
+# This was originally created to support only programming a bitstream into 7-Series or UltraScale+ devices.
+# Subsequently had minor changes to support programming a device image into a Versal AI Edge device, but
+# not renamed.
 
 if { $argc != 1 } {
-    puts [concat "Usage: " $argv0 " <bitstream_filename>"]
+    puts [concat "Usage: " $argv0 " <bitstream_or_device_image_filename>"]
     exit 1
 }
 
@@ -31,15 +36,19 @@ set NUM_TARGETS [llength [get_hw_targets -quiet]]
 if {$NUM_TARGETS == 1 } then {
     set TARGET [get_hw_targets]
     open_hw_target -quiet $TARGET
-    set DEVICE [get_hw_devices]
+    # Get the assumed single PL device on the JTAG chain, filtering out the ARM DAP on Versal devices
+    set DEVICE [get_hw_devices -filter {NAME !~ "arm_dap*"}]
     puts [concat "Programming " $bitstream_filename " into " $DEVICE ]
 
     # Select the single device to program 
     current_hw_device $DEVICE
     set_property PROGRAM.FILE $bitstream_filename [current_hw_device]
     program_hw_devices $DEVICE
+
+    # Display the PLM boot log for Versal devices. This is an empty property on devices which don't use a PLM.
+    puts [get_property PROGRAM.LOG [current_hw_device]]
 } else {
-    puts "Can only operate on a single target as no way of specifying the which target to load the bitstream to."
+    puts "Can only operate on a single target as no way of specifying the which target to load the bitstream or device image to."
     puts [concat "Number of targets : " $NUM_TARGETS]
 }
 
